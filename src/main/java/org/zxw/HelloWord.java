76120570396694c4958772abbd32d6c39b7d690c
zhuxiongw@126.com <zhuxiongw@126.com>
2017-08-10 18:43:51 +0800

    Initial commit

---
+++ src/main/java/org/zxw/HelloWord.java
@@ -0,0 +1,11 @@
+package org.zxw;
+
+/**
+ * Created by ZXW on 2017/8/10.
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
